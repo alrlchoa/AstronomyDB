2e74f3f65f51dd11c9792d77fd654bdd70d476c6
alrlchoa <alrlchoa@gmail.com>
2018-06-10 21:37:43 -0700

    buggy button
    Button is still buggy. Will fix.

--- astro/resources/views/pages/register.blade.php
+++ astro/resources/views/pages/register.blade.php
@@ -10,15 +10,45 @@
             <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
             <label for="inputConfPassword" class="sr-only">Confirm Password</label>
             <input type="password" id="inputConfPassword" class="form-control" placeholder="Confirm Password" required>
-            <div class="radio-inline mb-3">
-                <label>
-                <input type="radio" name="userType" value="astronomer"> Astronomer
-                <input type="radio" name="userType" value="researcher"> Researcher
-                </label>
+            <div class="btn-group btn-toggle"> 
+                <button class="btn btn-default" data-toggle="collapse" data-target="#Institution">Hide</button>
+                <button class="btn btn-primary active" data-toggle="collapse" data-target="#Institution">Show</button>
             </div>
+            <br />
+            <div class="well collapse" id="Institution">
             <label for="inputInstitution" class="sr-only">Institution</label>
             <input type="text" id="inputInstitution" class="form-control" placeholder="Institution" required>
+            </div>
             <br />
             <button class="btn btn-lg btn-secondary btn-block" type="submit">Log in</button>
             <p> Already have an account? <a href="/login">Log In</a></p>
+@endsection
+
+@section('add-scripts')
+    <script>
+    $('.btn-toggle').click(function() {
+        $(this).find('.btn').toggleClass('active');  
+        
+        if ($(this).find('.btn-primary').size()>0) {
+            $(this).find('.btn').toggleClass('btn-primary');
+        }
+        if ($(this).find('.btn-danger').size()>0) {
+            $(this).find('.btn').toggleClass('btn-danger');
+        }
+        if ($(this).find('.btn-success').size()>0) {
+            $(this).find('.btn').toggleClass('btn-success');
+        }
+        if ($(this).find('.btn-info').size()>0) {
+            $(this).find('.btn').toggleClass('btn-info');
+        }
+        
+        $(this).find('.btn').toggleClass('btn-default');
+        
+    });
+
+    $('form').submit(function(){
+        alert($(this["options"]).val());
+        return false;
+    });
+    </script>
 @endsection
