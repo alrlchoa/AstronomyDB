3c673e55c4679b97899fd6cdfe56a70e36b71b32
Alexander Larbi <j0j0b@ugrad.cs.ubc.ca>
2018-06-13 17:35:37 -0700

    fixing the branches

--- astro/resources/views/cb/show.blade.php
+++ astro/resources/views/cb/show.blade.php
@@ -7,7 +7,7 @@
     <h1>{{ $cb->id }}</h1>
 
     <p class = "lead">{{ $cb->name }}</p>
-    <p class = "lead">{{ $cbtype }}</p>
+    
 
     <p>Right Ascension: {{ $cb->right_ascension }}</p>
     <p>Declination: {{ $cb->declination }}</p>
