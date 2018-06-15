312ab25197453edde9e5cd58cd0e6300af5de989
Tanraj <tsaran70@gmail.com>
2018-06-14 17:37:28 -0700

    Show page

--- astro/resources/views/cb/show.blade.php
+++ astro/resources/views/cb/show.blade.php
@@ -3,47 +3,62 @@
 @section('title', '- View cb')
 
 @section('content')
+    <div class="row">
+        <div class="col-md-8">
+            <h1>Celestial Body {{ $cb->id }}</h1>
+            <hr>
 
-    <h1>Celestial Body {{ $cb->id }}</h1>
-    <hr>
+             <p class = "lead">{{ $cb->name }}</p>
 
-    <p class = "lead">{{ $cb->name }}</p>
+            <p><b>Right Ascension:</b> {{ $cb->right_ascension }}</p>
+            <p><b>Declination:</b> {{ $cb->declination }}</p>
+            <p><b>Verification Status:</b>
+            @if ($cb->verified == 1)
+                Verified
+            @else
+                Not Verified
+            @endif
+        </p>
+            @if (!empty($comet))
+                <p>Comet's Speed: {{$comet->speed}}</p>
+            @endif
 
-    <p><b>Right Ascension:</b> {{ $cb->right_ascension }}</p>
-    <p><b>Declination:</b> {{ $cb->declination }}</p>
-    <p><b>Verification Status:</b>
-    @if ($cb->verified == 1)
-        Verified
-    @else
-        Not Verified
-    @endif
-    </p>
-    @if (!empty($comet))
-        <p>Comet's Speed: {{$comet->speed}}</p>
-    @endif
+            @if (!empty($galaxy))
+                <p>Galaxy's Brightness: {{$galaxy->brightness}}</p>
+                <p>Galaxy's Redshift: {{$galaxy->redshift}}</p>
+                <p>Galaxy's Type: {{$galaxy->type}}</p>
+            @endif
 
-    @if (!empty($galaxy))
-        <p>Galaxy's Brightness: {{$galaxy->brightness}}</p>
-        <p>Galaxy's Redshift: {{$galaxy->redshift}}</p>
-        <p>Galaxy's Type: {{$galaxy->type}}</p>
-    @endif
+            @if (!empty($moon))
+                <p>Moon's Orbital Period: {{$moon->orbital_period}}</p>
+                <p>Moon's Radius: {{$moon->radius}}</p>
+                <p>Moon's Planet Id: {{$moon->planet_id}}</p>
+                <p>Planet's Orbital Period: {{$planetoid->orbital_period}}</p>
+                <p>Planet's Type: {{$planetoid->planet_type}}</p>
+            @endif
 
-    @if (!empty($moon))
-        <p>Moon's Orbital Period: {{$moon->orbital_period}}</p>
-        <p>Moon's Radius: {{$moon->radius}}</p>
-        <p>Moon's Planet Id: {{$moon->planet_id}}</p>
-        <p>Planet's Orbital Period: {{$planetoid->orbital_period}}</p>
-        <p>Planet's Type: {{$planetoid->planet_type}}</p>
-    @endif
+            @if (!empty($planet))
+                <p><b>Planet's Orbital Period: </b>{{$planet->orbital_period}}</p>
+                <p><b>Planet's Type: </b>{{$planet->planet_type}}</p>
+            @endif
 
-    @if (!empty($planet))
-        <p><b>Planet's Orbital Period: </b>{{$planet->orbital_period}}</p>
-        <p><b>Planet's Type: </b>{{$planet->planet_type}}</p>
-    @endif
-
-    @if (!empty($star))
-        <p>Star's Spectral Brightness: {{$spectral->spectral_type}}</p>
-        <p>Star's Brightness: {{$spectral->brightness}}</p>
-    @endif
+            @if (!empty($star))
+                <p>Star's Spectral Brightness: {{$spectral->spectral_type}}</p>
+                <p>Star's Brightness: {{$spectral->brightness}}</p>
+            @endif
+        </div>
+            <div class="col-md-4">
+                <div class="well">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <a href="#" class="btn btn-primary btm-block">Edit</a>
+                        </div>
+                        <div class="col-sm-6">
+                            <a href="#" class="btn btn-danger btm-block">Delete</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
 
 @endsection
