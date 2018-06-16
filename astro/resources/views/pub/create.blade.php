b8166fc8040bc4cf77ef2e92d0fec2bb652873e3
Alexander Larbi <j0j0b@ugrad.cs.ubc.ca>
2018-06-15 18:17:23 -0700

    Create Blade finished

--- astro/resources/views/pub/create.blade.php
+++ astro/resources/views/pub/create.blade.php
@@ -8,25 +8,21 @@
         <div class="col-md-12">
             <h1>Create a Publication</h1>
             <hr>
-            {!! Form::open(['route' => 'cb.store', 'files' => true]) !!}
+            {!! Form::open(['route' => 'pub.store', 'files' => true]) !!}
 
-            {{ Form::label('right_ascension', 'Right Ascension') }}
-            {!! Form::number('right_ascension',null,['class'=> 'form-control', 'placeholder' =>'Right Ascension', 'step'=>0.01]) !!}
-
-            {{ Form::label('declination', 'Declination') }}
-            {!! Form::number('declination',null,['class'=> 'form-control', 'placeholder' =>'Declination', 'step'=>0.01]) !!}
-
-            {{ Form::label('name', 'Name') }}
-            {!! Form::text('name',null,['class'=>'form-control','placeholder'=>'Name']) !!}
 
             <br />
-            
-            {{ Form::label('year', 'Year Discovered') }}
+
+            {{ Form::label('year', 'Year Published') }}
             {{ Form::selectYear('year', 0, 2018) }}
 
             <br />
+            <br />
 
+            {{ Form::label('doi', 'Digital Object Identifier') }}
+            {!! Form::text('doi',null,['class'=>'form-control','placeholder'=>'Digital Object Identifier']) !!}
 
+            <br />
 
 
             {!! Form::submit(isset($submitButtonText) ? $submitButtonText : 'Create', ['class' => 'btn btn-secondary']) !!}
