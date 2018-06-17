650f6f2682bc3cb58854c0d614b34ba7c62778a0
alexlarbi <alexlarbi47@gmail.com>
2018-06-17 11:02:22 -0700

    About Page

--- astro/resources/views/pages/about.blade.php
+++ astro/resources/views/pages/about.blade.php
@@ -7,20 +7,20 @@
             <div class="col-md-12">
             <h1>About AstroDB</h1>
             <hr>
-            <p>Small kitty warm kitty little balls of fur drink water out of the faucet but leave fur on owners clothes, 
-            lick face hiss at owner, pee a lot, and meow repeatedly scratch at fence purrrrrr eat muffins and poutine until 
-            owner comes back step on your keyboard while you're gaming and then turn in a circle so if it fits, i sits. Furrier 
-            and even more furrier hairball. Scratch leg; meow for can opener to feed me poop in a handbag look delicious and 
-            drink the soapy mopping up water then puke giant foamy fur-balls or steal the warm chair right after you get up 
-            need to chase tail attack dog, run away and pretend to be victim intently sniff hand. Nap all day climb a tree, 
-            wait for a fireman jump to fireman then scratch his face. Mrow. Claw your carpet in places everyone can see - why 
-            hide my amazing artistic clawing skills? pose purrfectly to show my beauty so catch mouse and gave it as a present 
-            lick the plastic bag or meowzer. Find empty spot in cupboard and sleep all day hack up furballs purrrrrr. Eat a plant, 
-            kill a hand cough hairball, eat toilet paper. Pee in human's bed until he cleans the litter box fish i must find my red 
-            catnip fishy fish yet lick sellotape. Human give me attention meow poop in litter box, scratch the walls flop over, yet 
-            sit and stare, so run outside as soon as door open and stuff and things. Twitch tail in permanent irritation mice yet cat 
-            ass trophy scratch at the door then walk away reward the chosen human with a slow blink do i like standing on litter cuz i 
-            sits when i have spaces, my cat buddies have no litter i live in luxury cat life attack dog, run away and pretend to be victim. </p>
+            <p> This is an astronomical database which includes two classes of users, Astronomer and the Researcher.
+                Query CB:
+                Astronomers are allowed to query celestial bodies in a number of ways, for instance, by entering certain coordinates for right ascension and declination. Results are shown on a new web-page displaying all of the celestial bodies at those coordinates in a table (Id, Name, Right-Ascension, Declination). From there, if the user clicks on the “View” button to the right of the celestial body, its details will be shown to the Astronomer on another web-page. These details include Right Ascension, Declination, Verification Status, Discoverer, Planet’s Orbital Period, Planet’s Type. All other celestial bodies this one is related to will also be displayed as well as the Publications it appears in (with publication details, Id, DOI, Date). In addition there are buttons to Edit and Delete the celestial body as well as Edit and View the publications. If the celestial body is a Star, Planet or Comet, there will be a button to Add Relation.
+                In addition to coordinates, the other fields on which the Astronomers can query a celestial body are brightness threshold, subclass of celestial body (with the option to include unverified celestial bodies) and Astronomical Id.
+                Query Users:
+                Aside from accessing celestial bodies, Astronomers are able to query other users by means of username and institution and other publications via Digital Object Identifier (DOI). The query results for another user are displayed in a table with columns Id, First Name, Last Name, Username, Researcher Status (if the user is a Researcher, the institution they are a fellow-of is also displayed).
+
+                CreateCB:
+                When an astronomer believes they have discovered a new celestial body, from the home-page they can click the CreateCB tab. This will lead them to the web-page for creating a celestial body (and inserting it into the database) provided that its Right Ascension and Declination combination is unique. The user must provide Right Ascension, Declination, and Name for the celestial body. There is the option to indicate whether or not it is verified as well as classify it specifically, or indicate that the celestial body is Not Specified. For each subclass of the celestial body, there are specific input fields for which if the details are known, can be inputted. Furthermore, on the same page, there is the mandatory form for indicating the Instrument Used, with input fields of Date of Discovery, Location, Instrument Model ID, and Type.
+
+                CreatePUB:
+                From the home page, the user can also create a publication by clicking on the “CreatePUB” tab at the top. This will lead them to a page on which they can input the publications Date of publication and Digital Object Identifier (DOI).
+                Researchers are able to do everything that an Astronomer can do, with the addition of some exclusive features. On the page for a celestial body, three more buttons are available to a Researcher -- “Verify”, “Edit Info” and “Delete”. If a Researcher clicks on “Verify”, then the Researcher will able to verify that there is a celestial body at the coordinates of interest. They will be brought to a separate page in which they must enter the DOI of a publication that verifies the celestial body. Researchers who click on the “Verify” button on an already verified celestial body can still use the button to input more publications. The “Edit Info” button turns all data fields pertaining to the celestial body’s attributes into text boxes. From here, the Researcher is able to edit attributes as long as the edits are valid. They can either save or cancel edits. When the user saves edits, the relevant tuples will be altered to reflect the new information. The “Delete” button will delete the celestial body and carry out any necessary actions following the deletion (for example, cascading, setting attributes in relations to null, etc).
+            </p>
             </div>
         </div>
 @endsection
